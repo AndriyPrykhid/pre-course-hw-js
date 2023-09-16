613227f6d1ef2437284aec09240f4ddbd9cb6d89
Andriy Prykhid <prihid7@gmail.com>
2023-09-16 18:04:03 +0300

    lesson1 task2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,6 @@
 // code
 let randomNumber = Math.floor(Math.random() * 100);
-console.log(randomNumber % 2 === 0)
+
 if (randomNumber % 2 === 0) {
     console.log(randomNumber, "Это четное число")
 
