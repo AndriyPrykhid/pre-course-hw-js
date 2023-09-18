d5186c055043c84d470535a7cde3814d10713971
Andriy Prykhid <prihid7@gmail.com>
2023-09-18 21:46:58 +0300

    lesson2 task05

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,7 +2,7 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-let passport2 = { ...passport };
-passport2.name = "Ivan"
+let passportCopy = { ...passport };
+passportCopy.name = "Ivan"
+console.log(passportCopy);
 console.log(passport);
-console.log(passport2);
