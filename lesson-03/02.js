b337b41750889fa33dc55b7a5c1758d79fdc1533
Andriy Prykhid <prihid7@gmail.com>
2023-09-24 17:18:21 +0300

    lesson03 task02

---
+++ lesson-03/02.js
@@ -0,0 +1,14 @@
+
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+
+function searchOrange(arr) {
+    if (arr.indexOf("апельсин") !== -1) {
+        return "«Ура! нашел"
+    } else {
+        return "Придется поискать в другом магазине…"
+    }
+}
+
+console.log(searchOrange(words))
+
